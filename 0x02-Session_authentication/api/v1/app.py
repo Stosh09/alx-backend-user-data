0bfda5ef4aa00770143252a54837e1489e9ae256
Stosh09 <stoshochieng@gmail.com>
2024-04-18 12:51:33 +0300

    Correct task 0

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -8,29 +8,25 @@ from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
 
+
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
 auth = None
-AUTH_TYPE = os.getenv('AUTH_TYPE')
-
-if AUTH_TYPE == 'auth':
-    from api.v1.auth.auth import Auth
-    auth = Auth()
-
-elif AUTH_TYPE == 'basic_auth':
+if os.getenv('AUTH_TYPE') == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
-
-elif AUTH_TYPE == 'session_auth':
+elif os.getenv('AUTH_TYPE') == 'auth':
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+elif os.getenv('AUTH_TYPE') == 'session_auth':
     from api.v1.auth.session_auth import SessionAuth
     auth = SessionAuth()
-
-elif AUTH_TYPE == 'session_exp_auth':
+elif os.getenv('AUTH_TYPE') == 'session_exp_auth':
     from api.v1.auth.session_exp_auth import SessionExpAuth
     auth = SessionExpAuth()
-
-elif AUTH_TYPE == 'session_db_auth':
+elif os.getenv('AUTH_TYPE') == 'session_db_auth':
     from api.v1.auth.session_db_auth import SessionDBAuth
     auth = SessionDBAuth()
 
@@ -43,39 +39,39 @@ def not_found(error) -> str:
 
 
 @app.errorhandler(401)
-def unauthorized(error) -> str:
-    """ Unauthorized request
+def request_unauthorized(error) -> str:
+    """ Request unauthorized handler
     """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
-def forbidden(error) -> str:
-    """ Forbidden request
+def request_forbidden(error) -> str:
+    """
+    Request where the user is authenticated
+    but not allowed to access to a resource
     """
     return jsonify({"error": "Forbidden"}), 403
 
 
 @app.before_request
-def before_request() -> str:
-    """ All before execute the main request
+def request_validation() -> str:
     """
-    exclude_paths = ['/api/v1/status/',
-                     '/api/v1/unauthorized/',
-                     '/api/v1/forbidden/',
-                     '/api/v1/auth_session/login/']
-
-    if auth is not None and \
-            auth.require_auth(request.path, exclude_paths) is True:
-
-        if auth.authorization_header(request) is None \
-                and auth.session_cookie(request) is None:
-            abort(401)
-
-        if auth.current_user(request) is None:
-            abort(403)
-
-        request.current_user = auth.current_user(request)
+    Performs request validation
+    """
+    if auth is None:
+        return
+    excluded_paths = ['/api/v1/status/',
+                      '/api/v1/unauthorized/', '/api/v1/forbidden/',
+                      '/api/v1/auth_session/login/']
+    if auth.require_auth(request.path, excluded_paths) is False:
+        return
+    if auth.authorization_header(request) is None and \
+            auth.session_cookie(request) is None:
+        abort(401)
+    if auth.current_user(request) is None:
+        abort(403)
+    request.current_user = auth.current_user(request)
 
 
 if __name__ == "__main__":
