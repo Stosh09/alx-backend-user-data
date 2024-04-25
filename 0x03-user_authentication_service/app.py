748e6f240eb549b81786e3e57c9f7c7123c876df
Stosh09 <stoshochieng@gmail.com>
2024-04-25 12:00:41 +0300

    Correction task 7

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -9,5 +9,14 @@ from auth import Auth
 app = Flask(__name__)
 AUTH = Auth()
 
+
+@app.route("/", methods=["GET"], strict_slashes=False)
+def index() -> str:
+    """GET /
+    Return:
+        - The home page's payload.
+    """
+    return jsonify({"message": "Bienvenue"})
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
